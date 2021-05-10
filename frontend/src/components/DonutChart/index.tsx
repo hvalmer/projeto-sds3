359d9fc575ce589f395ae11b6c1bbbef8492bb0e
Harlan Valmer <hvalmer@gmail.com>
2021-05-10 00:55:02 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,14 +24,6 @@ const DonutChart = () => {
         });
     }, []);
 
-    
-
-    //a lógica do DonutChart
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-    
     const options = {
         legend: {
             show: true
